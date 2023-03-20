8d395832265a1024065547db3c15553feeceff98
Tom Jackson <tomcat468@gmail.com>
2023-03-20 17:03:14 +0000

    Add homeHeader and homeTabs functions to homepage.js

--- src/homepage.js
+++ src/homepage.js
@@ -3,11 +3,39 @@ import restaurantImage from "./restaurant.jpg";
 const homePage = document.createElement("div");
 homePage.setAttribute("class", "homepage");
 
-function homeTitle() {
+function homeHeader() {
+  const header = document.createElement("div");
+  header.setAttribute("id", "header");
+  homeTitle(header);
+  homeTabs(header);
+  homePage.appendChild(header);
+}
+
+function homeTitle(head) {
   const Title = document.createElement("h1");
   Title.setAttribute("id", "home-title");
   Title.textContent = "Amazing Restaurant";
-  homePage.appendChild(Title);
+  head.appendChild(Title);
+}
+
+function homeTabs(head) {
+  const tabBar = document.createElement("div");
+  tabBar.setAttribute("id", "tab-bar");
+  for (let i = 0; i < 3; i++) {
+    const tab = document.createElement("button");
+    tab.setAttribute("id", `tab${i + 1}`);
+    if (i == 0) {
+      tab.textContent = "Home";
+    } else if (i == 1) {
+      tab.textContent = "Menu";
+    } else if (i == 2) {
+      tab.textContent = "Contact Us";
+    } else {
+      console.log("homeTabs if statement went wrong");
+    }
+    tabBar.appendChild(tab);
+  }
+  head.appendChild(tabBar);
 }
 
 function homeImage() {
@@ -26,7 +54,7 @@ function homeDescription() {
 }
 
 function createHomepage() {
-  homeTitle();
+  homeHeader();
   homeImage();
   homeDescription();
   content.appendChild(homePage);
