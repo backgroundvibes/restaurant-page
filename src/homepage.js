5946aeba5ddce162f88e9744fa29cdae5e31dfab
Tom Jackson <tomcat468@gmail.com>
2023-03-21 20:31:09 +0000

    Add quotes to homepage

--- src/homepage.js
+++ src/homepage.js
@@ -64,9 +64,18 @@ function homeQuoteSection() {
 }
 
 function homeQuotes(appendee) {
+  const quotes = [
+    "From the moment I walked in, I knew I was in for a treat. The atmosphere was warm and inviting, and the service was impeccable. I couldn't wait to see what was on the menu, and I was not disappointed. This restaurant is a must-visit for anyone who loves great food and a great experience. -ChatGPT",
+    "The flavors in every dish were exquisite, and the presentation was a work of art. Each bite was like a journey through a symphony of tastes and textures. This restaurant is truly a hidden gem that deserves to be discovered. -ChatGPT",
+    "I've been to many restaurants in my time, but this one stands out as a true standout. The attention to detail is evident in everything from the décor to the food. This is a place where you can truly relax and enjoy a meal that is both satisfying and memorable. -ChatGPT",
+    "This restaurant exceeded all of my expectations. The staff was friendly and attentive, and the food was out of this world. Every dish was prepared with care and attention to detail, and the flavors were perfectly balanced. I can't wait to come back and try more! -ChatGPT",
+    "If you're looking for a dining experience that is both sophisticated and fun, this restaurant is the perfect choice. The menu is diverse and innovative, with something to please every palate. I can't recommend this place enough. -ChatGPT",
+    "I was blown away by the quality of the food and the service at this restaurant. It's clear that the chef and staff take great pride in what they do, and it shows in every dish. This is the kind of restaurant that you'll want to come back to again and again. -ChatGPT",
+  ];
   for (let i = 0; i < 6; i++) {
     let quote = document.createElement("p");
     quote.setAttribute("id", `quote${i + 1}`);
+    quote.textContent = `${quotes[i]}`;
     appendee.appendChild(quote);
   }
 }
