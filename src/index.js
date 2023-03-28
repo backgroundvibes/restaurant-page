c60a11656672793447be20073af7652c83d7b71a
Tom Jackson <tomcat468@gmail.com>
2023-03-28 17:53:27 +0100

    Complete tab switching logic in index.js

--- src/index.js
+++ src/index.js
@@ -6,18 +6,39 @@ import "./style.css";
 const content = document.querySelector("#content");
 
 createHomepage();
+const tabBar = document.querySelectorAll("#tab-bar");
 
-const tabs = document.querySelectorAll("#tab-bar");
-tabs.forEach((tab) => {
-  tab.addEventListener("click", (e) => {
-    let toRemove = tab.closest("#content > div");
-    toRemove.remove();
-    if (e.target.textContent == "Home") {
-      createHomepage();
-    } else if (e.target.textContent == "Menu") {
-      createMenuPage();
-    } else if (e.target.textContent == "Contact Us") {
-      createContactPage();
-    }
+const eventListenAdd = () => {
+  let tabs = document.querySelectorAll("#tab-bar");
+  tabs.forEach((tab) => {
+    tab.addEventListener("click", pageGenerate);
   });
-});
+  let removeEventListen = () => {
+    tabs.forEach((tab) => {
+      tab.removeEventListener("click", pageGenerate);
+    });
+  };
+  return { removeEventListen };
+};
+
+eventListenAdd();
+
+function pageGenerate(e) {
+  let toRemove = content.firstElementChild;
+  toRemove.remove();
+  if (e.target.textContent == "Home") {
+    eventListenAdd.removeEventListen;
+    createHomepage();
+    eventListenAdd();
+  } else if (e.target.textContent == "Menu") {
+    eventListenAdd.removeEventListen;
+    createMenuPage();
+    eventListenAdd();
+  } else if (e.target.textContent == "Contact Us") {
+    eventListenAdd.removeEventListen;
+    createContactPage();
+    eventListenAdd();
+  }
+}
+
+function removeEventListen() {}
